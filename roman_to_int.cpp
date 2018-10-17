cb3c41ced99f13dd60185562461923c0fd15cb86
faradayin <yinfeng.v@gmail.com>
2018-10-17 21:09:55 +0800

    roman to int

---
+++ roman_to_int.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char, int> m{{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50},{'C', 100}, {'D', 500}, {'M', 1000}};
+        int sum = 0;
+        for(int i = 0;i<s.size();i++)
+        {
+            int val = m[s[i]];
+            if(m[s[i]] < m[s[i+1]] && i<s.size()-1) 
+                val = -val;
+            sum += val;
+        }
+        return sum;
+    }
+};

87a213231180747601921dda2aa47d6ad679f391
faradayin <yinfeng.v@gmail.com>
2018-10-23 20:06:29 +0800

    3sum closest

---
+++ 3sum_closest.cpp
@@ -0,0 +1,45 @@
+int threeSumClosest(vector<int>& nums, int target)
+{
+    sort(nums.begin(), nums.end());
+
+    int md = dis(nums[0]+nums[1]+nums[-1], target);
+    int sum = nums[0]+nums[1]+nums[nums.size()-1];//虽然数组下标可以是负数，但不能越界
+
+    for(size_t i = 0;i < nums.size()-2; i++)
+    {
+        size_t j = i+1;
+        size_t k = nums.size()-1;
+
+        while(j < k)
+        {
+            if(nums[i]+nums[j]+nums[k] == target)
+                return target;
+            else
+            {   
+                if(md > dis(nums[i]+nums[j]+nums[k], target))
+                {
+                    sum = nums[i]+nums[j]+nums[k];
+                    md = dis(nums[i]+nums[j]+nums[k], target);
+                }
+ 
+
+                if(nums[i]+nums[j]+nums[k] < target)
+                    j++;
+                else 
+                    k--;
+
+            }
+
+        }
+        
+    }
+    return sum;
+}
+
+int dis(int x, int y)
+{
+    if(x < y)
+        return y-x;
+    else 
+        return x-y;
+}

a926315feebd440e0dc7b660e4fb9b28b78e9cdd
faradayin <yinfeng.v@gmail.com>
2019-09-26 22:40:15 +0800

    lru cache

--- 146_lru_cache.cpp
+++ 146_lru_cache.cpp
@@ -1,26 +1,43 @@
+#include <unordered_map>
+#include <list>
+using std::list;
+using std::unordered_map;
 
 class LRUCache {
-    private:
-    unordered_map<int, int> ma;
-    int capacity;
-    list<int> l;
+private:
+    int size;
+    unordered_map<int, int> ma;// key ->value
+
+    list<int> que;// 维护一个key的队列
+    unordered_map<int, list<int>::iterator> mp;// key -> iterator
+    
+    void updateLRU(int key)
+    {
+        if(ma.count(key)) que.erase(mp[key]);
+        que.push_front(key);
+        mp[key] = que.begin();
+    }
+
+    void dump()
+    {
+        mp.erase(que.back());
+        ma.erase(que.back());
+        que.pop_back();
+    }
 public:
-    LRUCache(int capacity) {
-        
+    LRUCache(int capacity) : size(capacity) {}
+
+    int get(int key) 
+    {
+        if(ma.count(key) == 0) return -1;
+        updateLRU(key);
+        return ma[key];
     }
     
-    int get(int key) {
-        
-    }
-    
-    void put(int key, int value) {
-        
+    void put(int key, int value) 
+    {
+        if(ma.size() == size && ma.count(key) == 0) dump();
+        ma[key] = value;
+        updateLRU(key);
     }
 };
-
-/**
- * Your LRUCache object will be instantiated and called as such:
- * LRUCache* obj = new LRUCache(capacity);
- * int param_1 = obj->get(key);
- * obj->put(key,value);
- */

4fd39913ab3bf3b3db3977f9c1857812d642f68b
faradayin <yinfeng.v@gmail.com>
2019-11-01 14:42:06 +0800

    rename

--- renames.py
+++ renames.py
@@ -1,32 +1,35 @@
 import os
 import re
-path = 'E:\\feng\\lc'
-files = os.listdir(path);
 
-s = r'\d*'
+path = 'E:\\feng\\lc\\'
+files = os.listdir(path)
+
+s = r'^\d'
 for f in files:
-    if re.match(s, f):
-        print(f)
+    if re.match(s, f):#合法
+        pass
+       # print(f)
+    # f不合法
     else:
-        t = f
-        f.replace('_', ' ')
-        titles = open('C:\\Users\\Feng\\Desktop\\problems.txt')
-        line = titles.readline()
-        while True:
-            last = line
-            line = titles.readline()
-            if f == (line+'.cpp'):
-                f = last + f
-                #print(f)
+        t = f.replace('_', ' ')
+
+        titles = open(r'C:\Users\Feng\PycharmProjects\hello\problems.txt', 'r', encoding='utf-8',errors='ignore')
+        number = '0'
+        for name in titles:
+            name = name.strip().lower()
+            if t == (name + '.cpp'):  # 找到
+                os.rename(path + f, path + number + '_' + f)
+                print(f)
+
                 break
-            if len(line)==0: 
-                break
+            number = name
         titles.close()
-        #os.rename(os.path.join(path, t), os.path.join(path, f))
-        print(f)
-        
-                
-            
-            
-            
+
+        # os.rename(os.path.join(path, t), os.path.join(path, f))
+        #print(f)
+
+
+
+
+
 
